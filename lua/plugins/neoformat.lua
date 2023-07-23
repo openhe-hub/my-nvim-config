f6d777ddbcee822c4f7e713e20d816d3af011287
openhe-hub <625529334@qq.com>
2023-07-23 16:06:28 +0800

    fix: neoformat formatter name

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -3,18 +3,18 @@ local g = vim.g
 g.neoformat_verbose = 1
 
 -- lua formatter
-g.neoformat_enabled_lua = {'lua-format'}
+g.neoformat_enabled_lua = {'luaformat'}
 g.neoformat_lua_luaformat = {
-    exe = 'lua-format',
-    args = {'--indent-width=2'},
-    stdin = 1
+  exe = 'lua-format',
+  args = {'--indent-width=2'},
+  stdin = 1
 }
 
 -- clang format
-g.neoformat_enabled_cpp = {'clang-format'}
+g.neoformat_enabled_cpp = {'clangformat'}
+g.neoformat_enabled_c = {'clangformat'}
 g.neoformat_cpp_clangformat = {
-    exe = 'clang-format',
-    args = {'-style=Chromium'},
-    stdin = 1
+  exe = 'clang-format',
+  args = {'-style=Chromium'},
+  stdin = 1
 }
-
--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   int i = 1;
   cout << "hello world" << endl;
   cout << "i = " << i << endl;
