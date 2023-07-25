84cdd5207b75f2d6a2688ffe02dbe387b82cd21c
openhe-hub <625529334@qq.com>
2023-07-25 14:01:41 +0800

    upd: add more go-to-def function

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -22,6 +22,12 @@ keymap.set("n", "<leader>f", ":Neoformat<CR>")
 -- go to definirion
 keymap.set("n", "gd" , "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
 keymap.set("n", "gh" , "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
+keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+keymap.set("n", "gdd", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+keymap.set("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>")
+keymap.set("n", "gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+keymap.set("n", "gn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 
 -- live server
 keymap.set("n", "<leader>ls", ":LiveServerStart<CR>")
